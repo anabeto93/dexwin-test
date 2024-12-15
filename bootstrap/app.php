637e1f3c66a5724df97b3b029e6f00252f394c8c
Richard Anabeto Opoku <anabeto93@gmail.com>
2024-12-15 18:04:26 +0000

    fixed the session issue.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,21 +3,28 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Session;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
         health: '/up',
         api: __DIR__.'/../routes/api.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->group('api', [
+        $middleware->web([
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
+            ShareErrorsFromSession::class,
+        ]);
+        
+        $middleware->api([
             \Illuminate\Http\Middleware\HandleCors::class
         ]);
-        $middleware->web(Session::class);
-        $middleware->api();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
